117888099e4273a6c9214657453924da1f2e49a4
Josue Gatica Odato <josuegaticaodato@gmail.com>
2026-08-04 23:11:31 -0300

    Cambio de paleta de colores

--- scraper/test2.py
+++
@@ -1,9 +0,0 @@
-from scraper import BRAVE_EXE, BRAVE_SESSION_DIR, CATEGORIAS_URL, PlaywrightFetcher
-
-
-with PlaywrightFetcher(BRAVE_SESSION_DIR, BRAVE_EXE, headless=False) as fetcher:
-    soup = fetcher.get_soup(CATEGORIAS_URL)
-    print(soup.title.get_text(strip=True) if soup.title else "Sin titulo")
-
-    with open("categorias.html", "w", encoding="utf-8") as f:
-        f.write(str(soup))
